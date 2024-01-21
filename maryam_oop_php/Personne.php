48632ba0b7f45c57c0a5774edb706c0327a223d2
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-22 00:41:11 +0100

    Exercice 7 : Composition

--- maryam_oop_php/Personne.php
+++ maryam_oop_php/Personne.php
@@ -1,65 +1,51 @@
 <?php
 
-abstract class Forme{
-    abstract function calculerSurface();
-}
+class Adresse{
+    private $rue;
+    private $ville;
+    private $pays;
 
-class Cercle extends Forme {
-    const pi = 3.14;
-    private $rayon;
-
-    function __get($name){
-        return "Rayon = ". $this->rayon;
+    function __construct($rue, $ville, $pays)
+    {
+        $this->rue = $rue;
+        $this->ville = $ville;
+        $this->pays = $pays;
     }
 
-    function __set($name, $value){
-        $this->rayon = $value;
-    }
-
-    function calculerSurface(){
-        $surfcace = self::pi * $this->rayon * $this->rayon;
-        return "La surface du cercle est : " . $surfcace . " m².";
-    }
-
-}
-
-$cercle = new Cercle();
-
-$rayon = 7.5;
-
-$cercle->__set($rayon, $rayon);
-
-echo $cercle->__get($rayon);
-echo "<br>";
-echo $cercle->calculerSurface();
-echo "<br> <br>";
-
-
-class Rectangle extends Forme {
-    private $longueur;
-    private $largeur;
-
-    function __getRec(){
-        return ("Longueur = ". $this->longueur . "<br>". "Largeur = ". $this->largeur);
-    }
-
-    function __set($longueur, $largeur){
-        $this->longueur = $longueur;
-        $this->largeur = $largeur;
-    }
-
-    function calculerSurface(){
-        $surface = $this->largeur * $this->longueur;
-        echo "La surface du rectangle est : ". $surface ." m².";
+    function __toString()
+    {
+        return "$this->rue, $this->ville, $this->pays";
     }
 }
 
-$rectangle = new Rectangle();
+class Personne
+{
 
-$longueur = 12;
-$largeur = 9;
-$rectangle->__set($largeur, $longueur);
-//echo $rectangle->__getLargeur();
-echo $rectangle->__getRec();
-echo "<br>";
-$rectangle->calculerSurface();
+    private $nom;
+    private $age;
+    private $genre;
+
+    private $adresse;
+
+    function __construct($nom, Adresse $adresse)
+    {
+
+        $this->nom = $nom;
+//        $this->age = $age;
+//        $this->genre = $genre;
+        $this->adresse = $adresse;
+    }
+
+    function Presenter()
+    {
+        echo "Bonjour, je m'appelle  $this->nom  , mon adresse est : $this->adresse";
+    }
+
+}
+
+$adresse = new Adresse("110, 03 blodjd", "Safi", "Maroc");
+
+$personne = new Personne("maryam", $adresse);
+
+echo $personne->Presenter();
+
