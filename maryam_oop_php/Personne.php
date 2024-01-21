27c571f29d681ae5b1edde9b9792f07672b1b8f0
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-21 14:42:19 +0100

    Exercice 4 : Méthode statique

--- maryam_oop_php/Personne.php
+++ maryam_oop_php/Personne.php
@@ -6,57 +6,47 @@ class Personne{
     private $age;
     private $genre;
 
-    function __getNom($nom){
-        return $this->nom;
-    }
-    function __getAge($age){
-        return $this->age;
-    }
-    function __getGenre($genre){
-        return $this->genre;
-    }
+    //    function __getNom($nom){
+    //        return $this->nom;
+    //    }
+    //    function __getAge($age){
+    //        return $this->age;
+    //    }
+    //    function __getGenre($genre){
+    //        return $this->genre;
+    //    }
+    //
+    //    function __setNom($nom, $value){
+    //        $this->nom = $value;
+    //    }
+    //    function __setAge($age, $value){
+    //        $this->age = $value;
+    //    }
+    //    function __setGenre($genre, $value){
+    //        $this->genre = $value;
+    //    }
 
-    function __setNom($nom, $value){
-        $this->nom = $value;
-    }
-    function __setAge($age, $value){
-        $this->age = $value;
-    }
-    function __setGenre($genre, $value){
-        $this->genre = $value;
-    }
 
+    static function obtenirAgeRetraite(){
+        return 65;
+    }
 }
-//class Etudiant extends Personne {
-//
-//    public $identifiantEtudiant;
-//
-//    function __construct($nom, $age, $genre, $identifiantEtudiant)
-//    {
-//        parent::__construct($nom, $age, $genre);
-//        $this->identifiantEtudiant = $identifiantEtudiant;
-//    }
-//
-//
-//    function Present(){
-//        echo "Bonjour, je m'appelle  $this->nom , j'ai $this->age , genre:$this->genre, id: $this->identifiantEtudiant" ;
-//
-//    }
-//}
 
 $nom = "maryam";
 $age = 20;
 $genre = "feminin";
 
 $personne = new Personne();
+//
+//$personne->__setNom($nom, $nom);
+//$personne->__setAge($age, $age);
+//$personne->__setGenre($genre, $genre);
+//
+//echo "Bonjour,  ";
+//echo "je m'appelle " . $personne->__getNom($nom) . " , ";
+//echo "j'ai " . $personne->__getAge($age) . " ans , ";
+//echo "le genre : " . $personne->__getGenre($genre) . " . ";
 
-$personne->__setNom($nom, $nom);
-$personne->__setAge($age, $age);
-$personne->__setGenre($genre, $genre);
+//echo Personne::obtenirAgeRetraite();
 
-echo "Bonjour,  ";
-echo "je m'appelle " . $personne->__getNom($nom) . " , ";
-echo "j'ai " . $personne->__getAge($age) . " ans , ";
-echo "le genre : " . $personne->__getGenre($genre) . " . ";
-
-
+echo "L' âge de retraite est ". Personne::obtenirAgeRetraite() ." ans !";
