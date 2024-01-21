fb411a4618a24d0e63330fe88e0cd7f1ab29ce39
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-21 14:05:57 +0100

    Exercice 3 : Encapsulation

--- maryam_oop_php/Personne.php
+++ maryam_oop_php/Personne.php
@@ -2,50 +2,61 @@
 
 class Personne{
 
-    protected $nom;
-    protected $age;
-    protected $genre;
+    private $nom;
+    private $age;
+    private $genre;
 
-    function __construct($nom, $age, $genre){
-
-        $this->nom = $nom;
-        $this->age = $age;
-        $this->genre = $genre;
-
+    function __getNom($nom){
+        return $this->nom;
+    }
+    function __getAge($age){
+        return $this->age;
+    }
+    function __getGenre($genre){
+        return $this->genre;
     }
 
-    function Presenter(){
-        echo "Bonjour, je m'appelle  $this->nom , j'ai $this->age , genre:$this->genre" ;
+    function __setNom($nom, $value){
+        $this->nom = $value;
+    }
+    function __setAge($age, $value){
+        $this->age = $value;
+    }
+    function __setGenre($genre, $value){
+        $this->genre = $value;
     }
 
 }
+//class Etudiant extends Personne {
+//
+//    public $identifiantEtudiant;
+//
+//    function __construct($nom, $age, $genre, $identifiantEtudiant)
+//    {
+//        parent::__construct($nom, $age, $genre);
+//        $this->identifiantEtudiant = $identifiantEtudiant;
+//    }
+//
+//
+//    function Present(){
+//        echo "Bonjour, je m'appelle  $this->nom , j'ai $this->age , genre:$this->genre, id: $this->identifiantEtudiant" ;
+//
+//    }
+//}
 
-class Etudiant extends Personne {
-
-    public $identifiantEtudiant;
-
-    function __construct($nom, $age, $genre, $identifiantEtudiant)
-    {
-        parent::__construct($nom, $age, $genre);
-        $this->identifiantEtudiant = $identifiantEtudiant;
-    }
-
-
-    function Present(){
-        echo "Bonjour, je m'appelle  $this->nom , j'ai $this->age , genre:$this->genre, id: $this->identifiantEtudiant" ;
-
-    }
-}
-
-$identifiantEtudiant = 1;
 $nom = "maryam";
 $age = 20;
 $genre = "feminin";
 
-//$personne = new Personne($nom, $age, $genre);
-//$personne->Presenter();
+$personne = new Personne();
 
-$etudiant = new  Etudiant($nom, $age, $genre, $identifiantEtudiant);
-$etudiant->Present();
+$personne->__setNom($nom, $nom);
+$personne->__setAge($age, $age);
+$personne->__setGenre($genre, $genre);
 
-?>
+echo "Bonjour,  ";
+echo "je m'appelle " . $personne->__getNom($nom) . " , ";
+echo "j'ai " . $personne->__getAge($age) . " ans , ";
+echo "le genre : " . $personne->__getGenre($genre) . " . ";
+
+
